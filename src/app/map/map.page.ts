d8a715548cffdaac912f2cddd06ba290e626e12a
nkilders <noah.kilders@gmail.com>
2021-07-18 14:34:15 +0200

    Add Leaflet

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
+export class MapPage {
+  map: L.Map;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter() {
+    this.map = new L.Map('map', {
+      zoomControl: false
+    }).setView([0.0, 0.0], 0);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    L.control.scale().addTo(this.map);
   }
 
 }
