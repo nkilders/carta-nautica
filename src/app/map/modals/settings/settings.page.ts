6bf42512ef38f40033b1b4eb4eeeb4090f92a006
nkilders <noah.kilders@gmail.com>
2022-05-04 23:30:39 +0200

    Settings

--- src/app/map/modals/settings/settings.page.ts
+++ src/app/map/modals/settings/settings.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage {
+  speedUnit: string;
+  distanceUnit: string;
+  keepAwake: boolean;
+  language: string;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.speedUnit = 'km/h';
+    this.distanceUnit = 'km';
+    this.keepAwake = true;
+    this.language = 'de';
   }
 
 }
