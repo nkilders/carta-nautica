bbb43fb622126512315eadbc855673730698d35b
nkilders <noah.kilders@gmail.com>
2022-07-06 23:56:06 +0200

    Update Map Edit Page

--- src/app/pages/map-edit/map-edit.page.ts
+++ src/app/pages/map-edit/map-edit.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Map } from 'src/app/models/map.model';
+import { MapService } from 'src/app/services/map.service';
 
 @Component({
   selector: 'app-map-edit',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-edit.page.scss'],
 })
 export class MapEditPage implements OnInit {
+  @Input() map: Map;
 
-  constructor() { }
+  name: string = '';
+  url: string = '';
+  
+  constructor(
+    private modalCtrl: ModalController,
+    private mapSrv: MapService,
+  ) { }
 
   ngOnInit() {
+    this.name = this.map.name;
+    this.url = this.map.src;
   }
 
+  async saveHandler() {
+    if(!this.name) return;
+    if(!this.url) return;
+
+    this.map.name = this.name;
+    this.map.src = this.url;
+
+    await this.mapSrv.updateMap(this.map);
+
+    await this.closeModal();
+  }
+  
+  async closeModal() {
+    const modal = await this.modalCtrl.getTop();
+    await modal.dismiss();
+  }
 }
