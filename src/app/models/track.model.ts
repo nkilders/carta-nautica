412b60a2e3e47141d31754d9bee15250f1aa141c
nkilders <noah.kilders@gmail.com>
2022-07-10 20:54:52 +0200

    Update Track Model

--- src/app/models/track.model.ts
+++ src/app/models/track.model.ts
@@ -13,8 +13,28 @@ export class Track {
         this.name = name;
         this.points = points;
     }
+}
 
-    getCoordinatesForRendering() {
-        return this.points.map(point => point[1]);
+export function getCoordinatesForRendering(track: Track) {
+    return track.points.map(point => point[1]);
+}
+
+export function getTrackLength(track: Track) {
+    let length = 0;
+
+    for(let i = 1; i < track.points.length; i++) {
+        length += distance(track.points[i-1][1], track.points[i][1]);
     }
+
+    return length;
+}
+
+function distance(p1: Coordinate, p2: Coordinate) {
+    const p = 0.017453292519943295; // Math.PI / 180
+    const c = Math.cos;
+    const a = 0.5 - c((p2[1] - p1[1]) * p) / 2 + 
+            c(p1[1] * p) * c(p2[1] * p) * 
+            (1 - c((p2[0] - p1[0]) * p)) / 2;
+  
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
