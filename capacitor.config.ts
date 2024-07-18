db213742599b742bce12e762a0b97ef32f0cb232
nkilders <71071641+nkilders@users.noreply.github.com>
2024-07-18 20:31:25 +0200

    Update appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.nkilders.cartanautica',
   appName: 'carta-nautica',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
