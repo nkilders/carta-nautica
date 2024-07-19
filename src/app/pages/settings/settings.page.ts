ced1ed42de40ebc00b3ccdc0f24955633a54745a
nkilders <71071641+nkilders@users.noreply.github.com>
2024-07-19 23:14:54 +0200

    Structure of settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonListHeader, IonSelect, IonSelectOption, IonItem, IonLabel, IonToggle } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
@@ -9,13 +9,37 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TranslateModule, IonList, IonListHeader, IonSelect, IonSelectOption, IonItem, IonLabel, IonToggle]
 })
 export class SettingsPage implements OnInit {
+  public speedUnit = '0';
+  public distanceUnit = '0';
+  public mapPreloading = false;
+  public keepAwake = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadSettingsValues();
   }
 
+  private async loadSettingsValues() {
+    // TODO: load all values
+  }
+
+  protected updateSpeedUnit() {
+    // TODO: save new value
+  }
+
+  protected updateDistanceUnit() {
+    // TODO: save new value
+  }
+  
+  protected updateMapPreloading() {
+    // TODO: save new value
+  }
+  
+  protected updateKeepAwake() {
+    // TODO: save new value
+  }
 }
