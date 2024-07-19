4b465324c073c93289bc80a46c3e571b64f030d4
nkilders <71071641+nkilders@users.noreply.github.com>
2024-07-20 00:09:12 +0200

    Add settings service

---
+++ src/app/models/settings.ts
@@ -0,0 +1,18 @@
+export interface Settings {
+  speedUnit: SpeedUnit;
+  distanceUnit: DistanceUnit;
+  mapPreloading: boolean;
+  keepAwake: boolean;
+}
+
+export enum SpeedUnit {
+  KILOMETERS_PER_HOUR = 0,
+  METERS_PER_SECOND = 1,
+  KNOTS = 2,
+}
+
+export enum DistanceUnit {
+  KILOMETERS = 0,
+  METER = 1,
+  SEA_MILES = 2,
+}
---
+++ src/app/services/settings.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from './storage.service';
+import { DistanceUnit, Settings, SpeedUnit } from '../models/settings';
+
+const STORAGE_KEY = 'settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+  private settings?: Settings;
+
+  constructor(
+    private storage: StorageService,
+  ) { }
+
+  public async getAllSettings() {
+    await this.init();
+
+    return this.settings;
+  }
+
+  public async getSpeedUnit() {
+    await this.init();
+
+    return this.settings!.speedUnit;
+  }
+
+  public async setSpeedUnit(unit: SpeedUnit) {
+    await this.init();
+
+    this.settings!.speedUnit = unit;
+    // TODO: emit event
+
+    await this.save();
+  }
+
+  public async getDistanceUnit() {
+    await this.init();
+
+    return this.settings!.distanceUnit;
+  }
+
+  public async setDistanceUnit(unit: DistanceUnit) {
+    await this.init();
+
+    this.settings!.distanceUnit = unit;
+    // TODO: emit event
+
+    await this.save();
+  }
+
+  public async getMapPreloading() {
+    await this.init();
+
+    return this.settings!.mapPreloading;
+  }
+
+  public async setMapPreloading(preloading: boolean) {
+    await this.init();
+
+    this.settings!.mapPreloading = preloading;
+    // TODO: emit event
+
+    await this.save();
+  }
+
+  public async getKeepAwake() {
+    await this.init();
+
+    return this.settings!.keepAwake;
+  }
+
+  public async setKeepAwake(keepAwake: boolean) {
+    await this.init();
+
+    this.settings!.keepAwake = keepAwake;
+    // TODO: emit event
+
+    await this.save();
+  }
+
+  private async init() {
+    if(this.settings) {
+      return;
+    }
+
+    this.settings = await this.storage.get(STORAGE_KEY);
+
+    if(!this.settings) {
+      this.settings = this.defaultSettings();
+      await this.save();
+    }
+  }
+
+  private defaultSettings() : Settings {
+    return {
+      speedUnit: SpeedUnit.KILOMETERS_PER_HOUR,
+      distanceUnit: DistanceUnit.KILOMETERS,
+      mapPreloading: true,
+      keepAwake: true,
+    };
+  }
+
+  private async save() {
+    await this.storage.set(STORAGE_KEY, this.settings);
+  }
+}
