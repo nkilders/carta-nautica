1fdac40d749f206a5727c6b35c5d09a950c23f35
nkilders <71071641+nkilders@users.noreply.github.com>
2024-07-19 17:57:11 +0200

    Update app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'de.nkilders.cartanautica',
-  appName: 'carta-nautica',
+  appName: 'Carta Nautica',
   webDir: 'www',
 };
 
