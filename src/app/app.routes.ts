aa814d5abb502108659533ceef40efd6ad3626b4
nkilders <71071641+nkilders@users.noreply.github.com>
2024-07-26 22:48:42 +0200

    Add routes to every page for easier development and debugging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,37 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'layers',
+    loadComponent: () =>
+      import('./pages/layers/layers.page').then((m) => m.LayersPage),
+  },
+  {
+    path: 'layers-create',
+    loadComponent: () =>
+      import('./pages/layers-create/layers-create.page').then(
+        (m) => m.LayersCreatePage,
+      ),
+  },
+  {
+    path: 'layers-edit',
+    loadComponent: () =>
+      import('./pages/layers-edit/layers-edit.page').then(
+        (m) => m.LayersEditPage,
+      ),
+  },
+  {
     path: 'map',
     loadComponent: () => import('./pages/map/map.page').then((m) => m.MapPage),
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./pages/settings/settings.page').then((m) => m.SettingsPage),
+  },
+  {
+    path: 'tracks',
+    loadComponent: () =>
+      import('./pages/tracks/tracks.page').then((m) => m.TracksPage),
   },
   {
     path: '**',
