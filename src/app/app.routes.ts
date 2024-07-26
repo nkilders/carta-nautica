5da26a8b98810a829c258524046e56d01916252e
nkilders <71071641+nkilders@users.noreply.github.com>
2024-07-26 22:46:28 +0200

    Improve redirect on invalid route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'map',
-    pathMatch: 'full',
-  },
-  {
     path: 'map',
     loadComponent: () => import('./pages/map/map.page').then((m) => m.MapPage),
+  },
+  {
+    path: '**',
+    redirectTo: 'map',
   },
 ];
