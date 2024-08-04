9f89408c565c4f155089106d8c2cc34d2bc5df9d
nkilders <71071641+nkilders@users.noreply.github.com>
2024-08-04 13:35:35 +0200

    Add more details to tracks page

--- src/app/pages/tracks/tracks.page.ts
+++ src/app/pages/tracks/tracks.page.ts
@@ -12,13 +12,24 @@ import {
   IonLabel,
   IonButtons,
   IonButton,
+  IonNote,
+  IonCol,
 } from '@ionic/angular/standalone';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { Track } from 'src/app/models/tracks';
+import { Track, TrackWithoutId } from 'src/app/models/tracks';
 import { addIcons } from 'ionicons';
 import { ellipsisVertical } from 'ionicons/icons';
 import { TracksService } from 'src/app/services/tracks.service';
 import { ActionSheetController, AlertController } from '@ionic/angular';
+import { geoDistance } from 'src/app/coordinates';
+import { UnitService } from 'src/app/services/unit.service';
+import { DistanceUnit, SpeedUnit } from 'src/app/models/settings';
+
+interface DisplayTrack extends Track {
+  duration: string;
+  length: string;
+  averageSpeed: string;
+}
 
 @Component({
   selector: 'app-tracks',
@@ -26,6 +37,8 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
   styleUrls: ['./tracks.page.scss'],
   standalone: true,
   imports: [
+    IonCol,
+    IonNote,
     IonButton,
     IonButtons,
     IonLabel,
@@ -42,17 +55,12 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
   ],
 })
 export class TracksPage implements OnInit {
-  tracks: Track[] = [
-    {
-      id: 'asd',
-      name: 'My Track',
-      points: [],
-    },
-  ];
+  tracks: DisplayTrack[] = [];
 
   constructor(
     private tracksSrv: TracksService,
     private translate: TranslateService,
+    private unit: UnitService,
     private actionSheetCtrl: ActionSheetController,
     private alertCtrl: AlertController,
   ) {
@@ -113,6 +121,88 @@ export class TracksPage implements OnInit {
   }
 
   private async loadTracks() {
-    this.tracks = await this.tracksSrv.getAll();
+    const tracks = await this.tracksSrv.getAll();
+
+    this.tracks = [];
+
+    for (const track of tracks) {
+      const lengthKm = this.lengthKm(track);
+      const durationMin = this.durationMinutes(track);
+      const avgSpeedKmh = lengthKm / (durationMin / 60) || 0;
+
+      const length = await this.unit.convertDistance(
+        lengthKm,
+        DistanceUnit.KILOMETERS,
+      );
+      const avgSpeed = await this.unit.convertSpeed(
+        avgSpeedKmh,
+        SpeedUnit.KILOMETERS_PER_HOUR,
+      );
+
+      this.tracks.push({
+        ...track,
+        length: this.translate.instant('tracks.length', {
+          length: length.value.toFixed(1),
+          unit: this.unit.distanceUnitToText(length.unit),
+        }),
+        duration: this.translate.instant('tracks.duration', {
+          duration: this.formatDuration(durationMin),
+        }),
+        averageSpeed: this.translate.instant('tracks.avgSpeed', {
+          speed: avgSpeed.value.toFixed(1),
+          unit: this.unit.speedUnitToText(avgSpeed.unit),
+        }),
+      });
+    }
+  }
+
+  private formatDuration(durationMin: number) {
+    const hours = Math.floor(durationMin / 60);
+    const minutes = durationMin - hours * 60;
+
+    if (hours === 0) {
+      return `${minutes} min`;
+    } else {
+      return `${hours} h ${minutes} min`;
+    }
+  }
+
+  private lengthKm(track: TrackWithoutId) {
+    const { points } = track;
+
+    if (points.length <= 1) {
+      return 0;
+    }
+
+    let lengthKm = 0;
+
+    for (let i = 1; i < points.length; i++) {
+      const p1 = points[i - 1];
+      const p2 = points[i];
+
+      lengthKm += geoDistance(
+        p1.latitude,
+        p1.longitude,
+        p2.latitude,
+        p2.longitude,
+      );
+    }
+
+    return lengthKm;
+  }
+
+  private durationMinutes(track: TrackWithoutId) {
+    const { points } = track;
+
+    if (points.length <= 1) {
+      return 0;
+    }
+
+    const startMs = points[0].timestamp;
+    const endMs = points[points.length - 1].timestamp;
+
+    const durationMs = endMs - startMs;
+
+    return durationMs / 1000 / 60;
   }
 }
