08ef64605381ffbbb8a99200059de27bacf5eb3d
nkilders <71071641+nkilders@users.noreply.github.com>
2025-06-17 18:14:44 +0200

    Implement tracks edit page

--- src/app/pages/tracks-edit/tracks-edit.page.ts
+++ src/app/pages/tracks-edit/tracks-edit.page.ts
@@ -6,9 +6,17 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonList,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonButton,
 } from '@ionic/angular/standalone';
 import { Track } from 'src/app/models/tracks';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { ModalWrapper } from 'src/app/wrappers/modal-wrapper';
+import { TracksService } from 'src/app/services/tracks.service';
+import { AlertWrapper } from 'src/app/wrappers/alert-wrapper';
 
 @Component({
   selector: 'app-tracks-edit',
@@ -16,6 +24,11 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrls: ['./tracks-edit.page.scss'],
   standalone: true,
   imports: [
+    IonButton,
+    IonLabel,
+    IonInput,
+    IonItem,
+    IonList,
     IonContent,
     IonHeader,
     IonTitle,
@@ -29,7 +42,60 @@ export class TracksEditPage implements OnInit {
   @Input({ required: true })
   readonly track!: Track;
 
-  constructor() {}
+  protected name: string = '';
 
-  ngOnInit() {}
+  constructor(
+    private readonly modalCtrl: ModalWrapper,
+    private readonly alertCtrl: AlertWrapper,
+    private readonly translate: TranslateService,
+    private readonly tracks: TracksService,
+  ) {}
+
+  ngOnInit() {
+    this.name = this.track.name;
+  }
+
+  protected async saveChanges() {
+    if (this.name.trim().length === 0) {
+      await this.errorToast('tracksEdit.errorNameEmpty');
+      return;
+    }
+
+    const updatedTrack: Track = {
+      ...this.track,
+      name: this.name,
+    };
+
+    await this.tracks.update(this.track.id, updatedTrack);
+
+    await this.closeModal();
+  }
+
+  protected async closeModal() {
+    const modal = await this.modalCtrl.getTop();
+
+    if (!modal) {
+      return;
+    }
+
+    await modal.dismiss();
+  }
+
+  private async errorToast(textKey: string) {
+    const headerText = await this.translate.instant('tracksEdit.errorHeader');
+    const messageText = await this.translate.instant(textKey);
+    const okText = await this.translate.instant('general.ok');
+
+    const toast = await this.alertCtrl.create({
+      header: headerText,
+      message: messageText,
+      buttons: [
+        {
+          text: okText,
+        },
+      ],
+    });
+
+    await toast.present();
+  }
 }
